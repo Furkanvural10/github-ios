0d7acb67f6fdea978687ab8314b1469eb607c0ec
Furkan Vural <furkanvural52@gmail.com>
2023-12-19 14:15:19 +0300

    Add project and initial setup view

---
+++ Github/Github/FavoritesListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListVC.swift
+//  Github
+//
+//  Created by furkan vural on 19.12.2023.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+
+}
---
+++ Github/Github/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  Github
+//
+//  Created by furkan vural on 19.12.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemFill
+    }
+    
+
+
+
+}
